2337b3062699a6da92b3d43248a7f54899b2a3be
siddharthjain468 <siddharth.jain468@gmail.com>
2024-03-04 01:04:30 +0530

    added restart

--- Game_1.py
+++ Game_1.py
@@ -1,13 +1,16 @@
 import pygame
 import time
 import random
+import sys
+
+
 pygame.font.init()
 WIDTH, HEIGHT = 1024, 768
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("test_game")
+pygame.display.set_caption("Game devlopment by learning")
 
-BG = pygame.transform.scale(pygame.image.load("bg_1.jpg"), (WIDTH,HEIGHT))
+# BG = pygame.transform.scale(pygame.image.load("bg_1.jpg"), (WIDTH,HEIGHT))
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 PLAYER_VEL = 5
@@ -18,8 +21,14 @@ STAR_VEL = 3
 FONT = pygame.font.SysFont("comicsans", 20)
 
 def draw(player, elapsed_time, stars):
-    WIN.blit(BG,(0,0))
-    time_text = FONT.render(f"Time:{int(elapsed_time//3600)}h:{int((elapsed_time%3600)//60)}m:{int(elapsed_time%60)}s", 1, "white")
+    # WIN.blit(BG,(0,0))
+    WIN.fill(pygame.Color((100, 100, 100)))
+    if int(elapsed_time//3600)!= 0:
+        time_text = FONT.render(f"Time:{int(elapsed_time//3600)}h:{int((elapsed_time%3600)//60)}m:{int(elapsed_time%60)}s", 1, "white")
+    elif int(elapsed_time%3600//60) != 0:
+        time_text = FONT.render(f"Time:{int((elapsed_time%3600)//60)}m:{int(elapsed_time%60)}s", 1, "white")
+    else:
+        time_text = FONT.render(f"Time:{int(elapsed_time%60)}s", 1, "white")
     WIN.blit(time_text, (WIDTH-200,10))
     pygame.draw.rect(WIN, "white", player)
     for star in stars:
@@ -32,6 +41,7 @@ def main():
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
+    global STAR_VEL,PLAYER_VEL
     
     star_add_increment = 2000
     star_count = 0
@@ -40,11 +50,12 @@ def main():
     stars = []
 
     while run:
-        star_count += clock.tick(60)
+        clock.tick(60)
+        star_count+=2
         elapsed_time = time.time() - start_time
 
         if star_count > star_add_increment:
-            for _ in range(3):
+            for _ in range(2):
                 star_x = random.randint(0, WIDTH - STAR_WIDTH)
                 star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
                 stars.append(star)
@@ -74,23 +85,47 @@ def main():
             WIN.blit(lost_text, (WIDTH/2-lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
             pygame.display.update()
             pygame.time.delay(1000)
-          
-            WIN.fill(pygame.Color("black"))
-            continue_or_not = FONT.render("1.Continue \n 2.Exit", 1, "white")
+
+            WIN.fill(pygame.Color((100, 100, 100)))
+            continue_or_not = FONT.render("1. Restart", 1, "white")
             WIN.blit(continue_or_not, (WIDTH/2-continue_or_not.get_width()/2, HEIGHT/2 - continue_or_not.get_height()/2))
+            continue_or_not_2 = FONT.render("2. EXIT", 1, "white")
+            WIN.blit(continue_or_not_2, ((WIDTH/2-continue_or_not.get_width()/2), HEIGHT/2 - continue_or_not.get_height()/2+40))
             pygame.display.update()
             # pygame.time.delay(4000)
 
             pygame.event.clear()
             while True:
+                # print("entered while")
                 event = pygame.event.wait()
-                if event.type == pygame.K_1:
-                    main()
-                    continue
-                if event.type == pygame.K_2:
-                    print("in else if")
-                    pygame.quit()
+                # if event.type == pygame.KEYDOWN:
+                if event.key== pygame.K_1 or event.key == pygame.K_SPACE:
+                    hit = False
+                    elapsed_time = 0
+                    start_time = time.time()
+                    PLAYER_VEL = 5
+                    STAR_VEL = 3
+                    stars = []
+                    star_add_increment = 2000
+                    star_count = 0
                     break
+                if event.key == pygame.K_2 or event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                    break
+
+# pygame.event.clear()
+# while True:
+#     event = pygame.event.wait()
+#     event = pygame.event.get()
+#     if event.type == QUIT:
+#         pygame.quit()
+#         sys.exit()
+#     elif event.type == KEYDOWN:
+#         if event.key = K_f:
+#             do something...
+
+
 
         draw(player, elapsed_time, stars)
     pygame.quit() 
